bc9c85d4a311e44a12282dd026626f869df813f4
thephantomcodes <daniel.stringer@hotmail.com>
2024-03-18 12:51:07 -0400

    Read sart weights from file.

--- src/main.cpp
+++ src/main.cpp
@@ -10,38 +10,38 @@
 #include <thread>
 #include "Scanner.h"
 
-void printSums(CT::Scanner& params)
+void printSums(CT::Scanner& scanner)
 {
   double row_sum_total = 0.0;
   double col_sum_total = 0.0;
 
-  std::cout << "Row Sums Size: " << params.row_sums.size() << "\n";
-  for(auto row_sum : params.row_sums)
+  std::cout << "Row Sums Size: " << scanner.row_sums.size() << "\n";
+  for(auto row_sum : scanner.row_sums)
   {
     // std::cout << row_sum << " ";
     row_sum_total += row_sum;
   }
   std::cout << "Row Sum Total: " << row_sum_total << " " << std::endl;
   
-  std::cout << "Col Sums Size: " << params.col_sums.size() << "\n";
-  for(auto  col_sum : params.col_sums)
+  std::cout << "Col Sums Size: " << scanner.col_sums.size() << "\n";
+  for(auto  col_sum : scanner.col_sums)
   {
     col_sum_total += col_sum;
   }
   std::cout << "Col Sum Total: " << col_sum_total << " " << std::endl;
   
-  std::cout << "Grand Total: " << params.grand_total << std::endl;
-  std::cout << "Row Diff: " << row_sum_total - params.grand_total << std::endl;
-  std::cout << "Col Diff: " << col_sum_total - params.grand_total << std::endl;
+  std::cout << "Grand Total: " << scanner.grand_total << std::endl;
+  std::cout << "Row Diff: " << row_sum_total - scanner.grand_total << std::endl;
+  std::cout << "Col Diff: " << col_sum_total - scanner.grand_total << std::endl;
 }
 
-void writeWeightData(std::string ofname, CT::Scanner& params)
+void writeWeightData(std::string ofname, CT::Scanner& scanner)
 {
   std::fstream ofs;
   ofs.open(ofname, std::fstream::out | std::fstream::binary);
-  ofs << params.grand_total;
-  for(auto row_sum : params.row_sums) ofs << row_sum;
-  for(auto col_sum : params.col_sums) ofs << col_sum;
+  ofs.write(reinterpret_cast<char*>(&scanner.grand_total), sizeof(double));
+  for(auto row_sum : scanner.row_sums) ofs.write(reinterpret_cast<char*>(&row_sum), sizeof(double));;
+  for(auto col_sum : scanner.col_sums) ofs.write(reinterpret_cast<char*>(&col_sum), sizeof(double));;
   ofs.close();
 }
 
@@ -82,18 +82,49 @@ void readFile(std::string fname, std::vector<double>& vec, int size)
   double buffer;
   for(int i=0; i<size; i++)
   {
-    fs.read(reinterpret_cast<char*>(&buffer), 8);
+    fs.read(reinterpret_cast<char*>(&buffer), sizeof(double));
     vec[i] = buffer;
+  }
+  fs.close();
+}
+
+void readSumFile(std::string fname, CT::Scanner &scanner)
+{
+  std::fstream fs;
+  fs.open(fname, std::fstream::in | std::fstream::binary);
+  if (!fs.is_open())
+  {
+    std::cerr << "Can't find input file " << fname << "\n";
+    exit(-1);
+  }
+  
+  double buffer;
+  fs.read(reinterpret_cast<char*>(&buffer), sizeof(double));
+  scanner.grand_total = buffer;
+
+  for(int i=0; i<scanner.num_pixels*scanner.num_pixels; i++)
+  {
+    fs.read(reinterpret_cast<char*>(&buffer), sizeof(double));
+    scanner.row_sums[i] = buffer;
+  }
+
+  for(int i=0; i<scanner.num_views*scanner.num_detectors; i++)
+  {
+    fs.read(reinterpret_cast<char*>(&buffer), sizeof(double));
+    scanner.col_sums[i] = buffer;
   }
   fs.close();
 }
 
 int main(int argc, const char* argv[])
 {
-  bool outputAll = false;
   int sysSize = (argc <= 1) ? 128 : std::atoi(argv[1]);
   double fov = (argc <= 2) ? 360.0 : (double)std::atof(argv[2]);
   char input_img = (argc <= 3) ? 'u' : *argv[3];
+  char operation = (argc <= 4) ? 'p' : *argv[4];
+  int sart_iter = (argc <= 5) ? 5 : std::atoi(argv[5]);
+  double relax_param = (argc <= 6) ? 1.0 : (double)std::atof(argv[6]);
+
   std::string in_file_prefix = "input/unit_disc_";
   std::string out_file_prefix = "output/sino_unit_disc_";
   std::string img_out_file_prefix = "output/img_unit_disc_";
@@ -105,24 +136,28 @@ int main(int argc, const char* argv[])
     img_out_file_prefix = "output/img_phantom_";
   }
   
-  auto params = CT::Scanner(50.0, 55.0, sysSize, sysSize, sysSize, 10.0, fov, 0.0);
-  int total_pixels = params.num_pixels*params.num_pixels;
-  int total_detectors = params.num_detectors*params.num_views;
+  auto scanner = CT::Scanner(50.0, 55.0, sysSize, sysSize, sysSize, 10.0, fov, 0.0);
+  int total_pixels = scanner.num_pixels*scanner.num_pixels;
+  int total_detectors = scanner.num_detectors*scanner.num_views;
   std::vector<double> img(total_pixels);
   std::vector<double> sinogram(total_detectors);
-  readFile(in_file_prefix + std::to_string(params.num_pixels) + ".dat", img, total_pixels);
+  readFile(in_file_prefix + std::to_string(scanner.num_pixels) + ".dat", img, total_pixels);
   sart_weight_prefix
-    .append(std::to_string(params.num_pixels))
+    .append(std::to_string(scanner.num_pixels))
     .append("_")
-    .append(std::to_string((int)params.field_of_view))
+    .append(std::to_string((int)scanner.field_of_view))
     .append(".dat");
 
 #ifdef GEN_SART_WEIGHTS
-  params.project(&img, &sinogram, 0, params.num_views, CT::ProjectionDirection::Forward);
-  printSums(params);
-  writeWeightData(sart_weight_prefix, params);
+  scanner.project(&img, &sinogram, 0, scanner.num_views, CT::ProjectionDirection::Forward);
+  printSums(scanner);
+  writeWeightData(sart_weight_prefix, scanner);
+  std::cout << "GT " << scanner.grand_total << " RS " << scanner.row_sums[0] << " CS " << scanner.col_sums[0] << "\n";
   return 0;
 #endif
+
+  readSumFile(sart_weight_prefix, scanner);
+  std::cout << "GT " << scanner.grand_total << " RS " << scanner.row_sums[0] << " CS " << scanner.col_sums[0] << "\n";
   
 ////////////////////////
 // Forward Projection
@@ -131,10 +166,10 @@ int main(int argc, const char* argv[])
   std::chrono::time_point<std::chrono::system_clock> start, end;
   start = std::chrono::system_clock::now();
   
-  std::thread t1(&CT::Scanner::project, params, &img, &sinogram, 0, params.num_views/4, CT::ProjectionDirection::Forward);
-  std::thread t2(&CT::Scanner::project, params, &img, &sinogram, params.num_views/4, params.num_views/2, CT::ProjectionDirection::Forward);
-  std::thread t3(&CT::Scanner::project, params, &img, &sinogram, params.num_views/2, 3*params.num_views/4, CT::ProjectionDirection::Forward);
-  std::thread t4(&CT::Scanner::project, params, &img, &sinogram, 3*params.num_views/4, params.num_views, CT::ProjectionDirection::Forward);
+  std::thread t1(&CT::Scanner::project, scanner, &img, &sinogram, 0, scanner.num_views/4, CT::ProjectionDirection::Forward);
+  std::thread t2(&CT::Scanner::project, scanner, &img, &sinogram, scanner.num_views/4, scanner.num_views/2, CT::ProjectionDirection::Forward);
+  std::thread t3(&CT::Scanner::project, scanner, &img, &sinogram, scanner.num_views/2, 3*scanner.num_views/4, CT::ProjectionDirection::Forward);
+  std::thread t4(&CT::Scanner::project, scanner, &img, &sinogram, 3*scanner.num_views/4, scanner.num_views, CT::ProjectionDirection::Forward);
   
   t1.join();
   t2.join();
@@ -145,44 +180,50 @@ int main(int argc, const char* argv[])
   std::chrono::duration<double> elapsed_seconds = end - start;
   std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
   
-  writePpmHeader(out_file_prefix + std::to_string(params.num_pixels) + ".ppm", params.num_detectors, params.num_views);
+  writePpmHeader(out_file_prefix + std::to_string(scanner.num_pixels) + ".ppm", scanner.num_detectors, scanner.num_views);
   double sino_max = *std::max_element(sinogram.begin(), sinogram.end());
-  writePpmData(out_file_prefix + std::to_string(params.num_pixels) + ".ppm", sinogram, total_detectors, sino_max, 0.0);
+  writePpmData(out_file_prefix + std::to_string(scanner.num_pixels) + ".ppm", sinogram, total_detectors, sino_max, 0.0);
   
 ////////////////////////
 // Ramp Filtering
 ////////////////////////
 
-  // start = std::chrono::system_clock::now();
-  // params.rampFilter(params.num_detectors, sinogram.data());
-  // end = std::chrono::system_clock::now(); 
-  // elapsed_seconds = end - start;
-  // std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+  if(operation == 'f')
+  {
+    start = std::chrono::system_clock::now();
+    scanner.rampFilter(scanner.num_detectors, sinogram.data());
+    end = std::chrono::system_clock::now(); 
+    elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+  }
   
 ////////////////////////
 // Back Projection
 ////////////////////////
 
-  start = std::chrono::system_clock::now();
-  
-  std::thread u1(&CT::Scanner::project, params, &img, &sinogram, 0, params.num_views/4, CT::ProjectionDirection::Backward);
-  std::thread u2(&CT::Scanner::project, params, &img, &sinogram, params.num_views/4, params.num_views/2, CT::ProjectionDirection::Backward);
-  std::thread u3(&CT::Scanner::project, params, &img, &sinogram, params.num_views/2, 3*params.num_views/4, CT::ProjectionDirection::Backward);
-  std::thread u4(&CT::Scanner::project, params, &img, &sinogram, 3*params.num_views/4, params.num_views, CT::ProjectionDirection::Backward);
-  
-  u1.join();
-  u2.join();
-  u3.join();
-  u4.join();
-  
-  end = std::chrono::system_clock::now(); 
-  elapsed_seconds = end - start;
-  std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
-  
-  writePpmHeader(img_out_file_prefix + std::to_string(params.num_pixels) + ".ppm", params.num_detectors, params.num_views);
-  double img_max = *std::max_element(img.begin(), img.end());
-  double img_min = *std::min_element(img.begin(), img.end());
-  writePpmData(img_out_file_prefix + std::to_string(params.num_pixels) + ".ppm", img, total_pixels, img_max, img_min);
+  if(operation == 'f' || operation == 'b')
+  {
+    start = std::chrono::system_clock::now();
+    
+    std::thread u1(&CT::Scanner::project, scanner, &img, &sinogram, 0, scanner.num_views/4, CT::ProjectionDirection::Backward);
+    std::thread u2(&CT::Scanner::project, scanner, &img, &sinogram, scanner.num_views/4, scanner.num_views/2, CT::ProjectionDirection::Backward);
+    std::thread u3(&CT::Scanner::project, scanner, &img, &sinogram, scanner.num_views/2, 3*scanner.num_views/4, CT::ProjectionDirection::Backward);
+    std::thread u4(&CT::Scanner::project, scanner, &img, &sinogram, 3*scanner.num_views/4, scanner.num_views, CT::ProjectionDirection::Backward);
+    
+    u1.join();
+    u2.join();
+    u3.join();
+    u4.join();
+    
+    end = std::chrono::system_clock::now(); 
+    elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+    
+    writePpmHeader(img_out_file_prefix + std::to_string(scanner.num_pixels) + ".ppm", scanner.num_detectors, scanner.num_views);
+    double img_max = *std::max_element(img.begin(), img.end());
+    double img_min = *std::min_element(img.begin(), img.end());
+    writePpmData(img_out_file_prefix + std::to_string(scanner.num_pixels) + ".ppm", img, total_pixels, img_max, img_min);
+  }
   
   return 0;
 }
