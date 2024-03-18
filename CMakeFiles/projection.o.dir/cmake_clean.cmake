d3d0b359d2951027506d1af40bab875f8a6c65e3
thephantomcodes <daniel.stringer@hotmail.com>
2024-03-17 21:41:27 -0400

    Move files to parent dir.

---
+++ CMakeFiles/projection.o.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projection.o.dir/src/Scanner.cpp.o"
+  "CMakeFiles/projection.o.dir/src/main.cpp.o"
+  "projection.o.pdb"
+  "projection.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projection.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
